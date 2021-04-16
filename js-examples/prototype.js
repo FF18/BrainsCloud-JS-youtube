21bc9fb95271b89f604f6be88997eeea58eb71ee
Felix <felixf4@gmail.com>
2021-04-16 20:13:32 +0300

    end first lesson prototype

--- js-examples/prototype.js
+++ js-examples/prototype.js
@@ -41,3 +41,66 @@
 // const cat = new Cat({name: 'Murzik', color: '#000', hasTail: true})
 // console.log(cat)
 
+
+
+// создание класса по новому ES6
+class Animal {
+    constructor(options) {
+        this.name = options.name
+        this.color = options.color
+    }
+
+    voice() {
+        console.log('Base voice from ', this.name)
+    }
+}
+
+const dog = new Animal({
+    name: 'Rex',
+    color: 'white'
+})
+class Cat extends Animal {
+    constructor(options) {
+        super(options)
+
+        this.hasTail = options.hasTail
+        this.type = 'cat'
+    }
+
+    voice() {
+        super.voice()
+        console.log(this.name + ' says myau')
+    }
+}
+
+const cat = new Cat({name: 'Murzik', color: '#000', hasTail: true})
+
+
+
+// Examples - использование прототипов 
+Object.prototype.print = function() {
+    console.log('I am object: ', this)
+}
+
+cat.print()
+
+Array.prototype.mapAndLog = function() {
+    console.log('Array to map', this)
+    return this.map.apply(this, arguments)
+}
+
+console.log([1, 2, 3, 4].mapAndLog(x => x ** 2))
+
+String.prototype.toTag = function(tagName) {
+    return `<${tagName}>${this}</${tagName}>`
+}
+console.log('eminem'.toTag('strong'))
+console.log('eminem'.toTag('em'))
+
+Number.prototype.toBigInt = function() {
+    return BigInt(this)
+}
+
+
+const number = 42
+console.log(number.toBigInt())
